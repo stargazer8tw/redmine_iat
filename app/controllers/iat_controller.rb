b0915e1f49b69074ae0cc26d29bd6d2c5fa83da8
stargazer8tw <stargazer.taiwan@gmail.com>
2014-12-28 13:58:59 +0800

    fix controller

--- app/controllers/iat_controller.rb
+++ app/controllers/iat_controller.rb
@@ -3,7 +3,7 @@ class IatController < ApplicationController
 
 #    menu_item :iat
 
-     before_filter :find_project
+#     before_filter :find_project
      before_filter :authorize, :only => [:index]
 
 #    def controller_name
@@ -11,12 +11,12 @@ class IatController < ApplicationController
 #    end
 
     def index
-#        @project = Project.find(params[:id])
-#        session[:project_id] = params[:id]
-        @iat = Iat.find(:all)# @project.iat
+        @project = Project.find(params[:id])
+        session[:project_id] = params[:id]
+#        @iat = Iat.find(:all)# @project.iat
     end
 
-        def find_project
+    def find_project
         @project = Project.find(params[:project_id])
     end
 end
