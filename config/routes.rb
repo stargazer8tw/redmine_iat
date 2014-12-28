8b6c5efc625978eca454d71cfe36ee0a4e1db77e
stargazer8tw <stargazer.taiwan@gmail.com>
2014-12-28 13:30:10 +0800

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get 'iat', :to => 'iat#index'
+get '/iat', :to => 'iat#index'
 #get 'iat/:id', :to => 'iat#index'
-get 'projects/:project_id/iat', :to => 'iat#index'
+get '/projects/:project_id/iat', :to => 'iat#index'
