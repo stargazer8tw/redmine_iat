39cedc89ea63b792084d0b74eb29f54c96a177cb
stargazer8tw <stargazer.taiwan@gmail.com>
2015-01-02 14:46:45 +0800

    change to require, and ink

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,2 +1,23 @@
-//= require d3/d3.min
-//= require nvd3/nv.d3.min
+require.config({
+    paths: {
+        'data.content': '../content',
+        'data.templates': '../templates',
+        'data.images': '../img',
+        // javascript library
+        jquery: 'jquery/jquery-2.1.3.min',
+        text: 'require/text',
+        underscore: 'underscore/underscore-min',
+        backbone: 'backbone/backbone-min',
+        // none amd compliance
+        d3: 'd3/d3.min',
+        nvd3: 'nvd3/nv.d3.min',
+        ink: 'ink/ink.min'
+    },
+    shim: {
+        nvd3: {
+            exports: 'nv',
+            deps: ['d3.global']
+        }
+    },
+    enforceDefine: true
+});
