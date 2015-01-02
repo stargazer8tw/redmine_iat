1aa58891da2d9f70096a4ddb6c77627a89593055
stargazer8tw <stargazer.taiwan@gmail.com>
2015-01-02 21:01:27 +0800

    fix router.js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -30,12 +30,5 @@ define([
     'router'
 ], function (Router) {
     "use strict";
-    var initialize = function () {
-        // Pass in our Router module and call it's initialize function
-        Router.initialize();
-    };
-
-    return {
-        initialize: initialize
-    };
+    Router.initialize();
 });
