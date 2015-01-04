370f03d7830c68e18d2d891576034a5ae7fd25f5
stargazer8tw <stargazer.taiwan@gmail.com>
2015-01-04 13:41:06 +0800

    fix route to data

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,8 +1,8 @@
 class DataController < ApplicationController
     unloadable
-    respond_to :json
+
     def assigned
         @issues_by_assigned_to = Issue.by_assigned_to(@project)
-        respond_with @issues_by_assigned_to
+        render json: @issues_by_assigned_to
     end
 end
