8b5e91a51ef1b3921b5afd49badc5cfcd37d4c38
stargazer8tw <stargazer.taiwan@gmail.com>
2015-01-11 10:26:01 +0800

    fix missing find project id in data controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,12 @@
 class DataController < ApplicationController
     unloadable
 
+    before_filter :find_project
+
+    def find_project
+        @project = Project.find(params[:project_id])
+    end
+
     def assigned
         @issues_by_assigned_to = Issue.by_assigned_to(@project)
         render json: @issues_by_assigned_to
@@ -10,4 +16,5 @@ class DataController < ApplicationController
         @issues_by_subproject = Issue.by_subproject(@project)
         render json: @issues_by_subproject
     end
+
 end
