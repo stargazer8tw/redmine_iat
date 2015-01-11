694f8b0aa41584fc6a13b470f2156236e7e59a4c
stargazer8tw <stargazer.taiwan@gmail.com>
2015-01-11 10:21:10 +0800

    add issue by subproject

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -5,4 +5,9 @@ class DataController < ApplicationController
         @issues_by_assigned_to = Issue.by_assigned_to(@project)
         render json: @issues_by_assigned_to
     end
+
+    def subproject
+        @issues_by_subproject = Issue.by_subproject(@project)
+        render json: @issues_by_subproject
+    end
 end
